539e2258bf87ed3e0cf5e452465bbbc3eab50b22
Dustin Nemes <39643802+some-things@users.noreply.github.com>
2022-09-05 23:40:11 -0500

    add document dependency values flag when generating docs

--- src/generate/generate.ts
+++ src/generate/generate.ts
@@ -1,7 +1,5 @@
 import { clone } from '../git';
-import { getApplicationHelmReleases } from './application';
-import { downloadHelmChart, extractHelmChart, generateHelmChartDoc } from './helmChart';
-import { getHelmRepositoryURL } from './helmRepository';
+import { getApplicationRepositoryApplicationNames } from './applicationRepository';
 
 export const generate = async (
   repository: string,
@@ -10,15 +8,17 @@ export const generate = async (
   helmChartRepositoryPath: string
 ) => {
   await clone(repository, path, branch);
-  await getApplicationHelmReleases(path, "gatekeeper");
-  const helmRepositoryURL = await getHelmRepositoryURL(
-    helmChartRepositoryPath,
-    "open-policy-agent.github.io-charts"
-  );
-  await downloadHelmChart("gatekeeper", "3.8.1", helmRepositoryURL);
-  await extractHelmChart("gatekeeper-3.8.1.tgz");
-  await generateHelmChartDoc(
-    "gatekeeper",
-    "https://open-policy-agent.github.io/gatekeeper/charts//gatekeeper-3.8.1.tgz"
-  );
+  const applications = await getApplicationRepositoryApplicationNames(path);
+  console.log(applications);
+  // await getApplicationHelmReleases(path, "gatekeeper");
+  // const helmRepositoryURL = await getHelmRepositoryURL(
+  //   helmChartRepositoryPath,
+  //   "open-policy-agent.github.io-charts"
+  // );
+  // await downloadHelmChart("gatekeeper", "3.8.1", helmRepositoryURL);
+  // await extractHelmChart("gatekeeper-3.8.1.tgz");
+  // await generateHelmChartDoc(
+  //   "gatekeeper",
+  //   "https://open-policy-agent.github.io/gatekeeper/charts//gatekeeper-3.8.1.tgz"
+  // );
 };

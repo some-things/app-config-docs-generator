e035acab9b7e953a656b2f47724d26c633501545
Dustin Nemes <39643802+some-things@users.noreply.github.com>
2022-10-25 11:43:51 -0500

    chore: update version to v1.0.0

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -8,7 +8,7 @@ export const program = new Command();
 program
   .name("app-config-docs-generator")
   .description("CLI to generate app config documentation")
-  .version("1.0.0");
+  .version("v1.0.0");
 
 program
   .command("generate")
